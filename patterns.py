4bac0b86c04ebdc5df34d6d13797dc6291d33d79
Mr. Mueller <bmueller@hstat.org>
2022-03-10 16:57:27 -0500

    student and average

--- patterns.py
+++ patterns.py
@@ -3,11 +3,11 @@ import pprint
 # functions
 
 def run_tests(path,student,slug,patterns):
-	# testLog = {
+	# test_log = {
 	#	'innerHTML': true,
 	# 	'querySelector': false
 	# }
-	testLog = {
+	test_log = {
 		"patterns": {},
 		"summary": {
 			"correct":0,
@@ -15,21 +15,21 @@ def run_tests(path,student,slug,patterns):
 		}
 	}
 	for pattern in patterns:
-		testLog["summary"]["total"] = testLog["summary"]["total"] + 1
-		testLog["patterns"][pattern] = False
+		test_log["summary"]["total"] = test_log["summary"]["total"] + 1
+		test_log["patterns"][pattern] = False
 
 		with open(f'{path}/{student}/{slug}', 'rt') as f:
 			lines = [line.rstrip('\n') for line in f]
 			for line in lines:
 				# print(line)
 				if pattern.lower() in line.lower():
-					if(testLog["patterns"][pattern] == False): # first time seeing pattern
-						testLog["summary"]["correct"] = testLog["summary"]["correct"] + 1
-					testLog["patterns"][pattern] = True
+					if(test_log["patterns"][pattern] == False): # first time seeing pattern
+						test_log["summary"]["correct"] = test_log["summary"]["correct"] + 1
+					test_log["patterns"][pattern] = True
 					# print('TRUE!!!')
-
-	pprint.pprint(testLog)
-	return(testLog)
+	test_log["summary"]["score"] = test_log["summary"]["correct"]/test_log["summary"]["total"]
+	# pprint.pprint(test_log)
+	return(test_log)
 
 # settings 
 cohort = 'js-2023'
@@ -68,7 +68,14 @@ rubric = [
 
 # run_tests(path,student,slug,patterns_01)
 def run_student_tests(path,student,rubric):
+	all_test_logs = []
 	for lesson in rubric:
-		run_tests(path,student,lesson["slug"],lesson["patterns"])
+		all_test_logs.append(run_tests(path,student,lesson["slug"],lesson["patterns"]))
+	# pprint.pprint(alltest_logs)
+	average = 0
+	for test_log in all_test_logs:
+		average = average + test_log["summary"]["score"]
+	average = average / len(all_test_logs)
+	return(average*10)
 
-run_student_tests(path,student,rubric)
+print(student + ": " + str(run_student_tests(path,student,rubric)))
