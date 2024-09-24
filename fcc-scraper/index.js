207b92ef15e4dfcf7acfe19cd73318acf20737e8
Samuel Sharivker <sam@ShmulPro.local>
2024-09-24 10:59:18 -0400

    creates csv file in api

--- fcc-scraper/index.js
+++ fcc-scraper/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const fs = require("fs");
+const path = require("path");
 
+const app = express();
 app.use(cors());
 
+const csvFilePath = path.join(__dirname, "data.csv");
+
 async function getCompletedAssignments(username) {
-  // Fetch in parallel for faster speeds
   const [getStudent, getMeta] = await Promise.all([
     fetch(
       `https://api.freecodecamp.org/api/users/get-public-profile?username=${username}`
@@ -20,7 +23,6 @@ async function getCompletedAssignments(username) {
     getMeta.json(),
   ]);
 
-  // Convert to map
   const challengesMap = meta.challengeOrder.reduce((map, challenge) => {
     map[challenge.id] = challenge.title;
     return map;
@@ -29,20 +31,58 @@ async function getCompletedAssignments(username) {
   const completedChallenges = student.entities.user[
     username
   ].completedChallenges.map((challenge) => {
-    // Replace timecode with formatted time
     challenge.completedDate = new Date(
       challenge.completedDate
     ).toLocaleDateString();
-
-    // Set the title of challenge using challengeMap title
     if (challengesMap[challenge.id]) {
       challenge.title = challengesMap[challenge.id];
     }
-
     return challenge;
   });
 
   return [completedChallenges, meta.challengeOrder];
+}
+
+function createCSVIfNotExists(challengeOrder) {
+  if (!fs.existsSync(csvFilePath)) {
+    const headers = [
+      "username",
+      ...challengeOrder.map((challenge) => challenge.title),
+      "completion rate",
+    ];
+    fs.writeFileSync(csvFilePath, headers.join(",") + "\n");
+  }
+}
+
+function appendStudentToCSV(username, completedChallenges, challengeOrder) {
+  const completedTitles = completedChallenges.map(
+    (challenge) => challenge.title
+  );
+  const row = [username];
+  let totalCompleted = 0;
+
+  challengeOrder.forEach((challenge) => {
+    if (completedTitles.includes(challenge.title)) {
+      row.push("1");
+      totalCompleted += 1;
+    } else {
+      row.push("0");
+    }
+  });
+
+  const totalChallenges = challengeOrder.length;
+  const completionRate = `${totalCompleted} / ${totalChallenges}`;
+  row.push(completionRate);
+
+  fs.appendFileSync(csvFilePath, row.join(",") + "\n");
+}
+
+function studentExistsInCSV(username) {
+  if (fs.existsSync(csvFilePath)) {
+    const csvData = fs.readFileSync(csvFilePath, "utf8");
+    return csvData.split("\n").some((row) => row.startsWith(username));
+  }
+  return false;
 }
 
 app.get("/", (req, res) => {
@@ -55,12 +95,14 @@ app.get("/", (req, res) => {
   }
 
   getCompletedAssignments(student)
-    .then((completedAssignments) => {
-      res.json({
-        student: student,
-        challenges: completedAssignments[0],
-        meta: completedAssignments[1],
-      });
+    .then(([completedChallenges, challengeOrder]) => {
+      createCSVIfNotExists(challengeOrder);
+
+      if (!studentExistsInCSV(student)) {
+        appendStudentToCSV(student, completedChallenges, challengeOrder);
+      }
+
+      res.json({ status: "Done!" });
     })
     .catch((err) => {
       res.status(500).json({ error: "Something went wrong!", details: err });
